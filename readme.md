77576cb66ea6636912af34bae455d44c5e0d97f7
6f5 <arashrestart@gmail.com>
2021-03-20 23:08:37 -0400

    Fixed laa character and unlinked characters

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # rtl-arabic
 
-
-
 Display arabic text from right to left in console/terminal. It's doesn't support the letters with punctuations yet. I'll complete it when I get time. The code is not the best but it works for now.
+
 # Exposed Functions
-  - rtlConverter
+
+- rtlConverter
 
 # Example!
 
@@ -14,7 +14,17 @@ const convertedText = rtlConverter("احبكم");
 console.log(convertedText); // احبكم
 ```
 
+## What's New?
+
+- Fixed the لا || laa character
+- Fixed unlinked characters
 
 # Todo:
-  - Add punctuation support
-  - Fix unlinked letters
+
+- Add Harakat Support
+- Add Numbers Support
+- Display both Arabic and English text correctly
+- Support Farsi
+- Support Farsi Numbers
+- Support English (I couldn't think of a scenario where you'd use that but I'll add it for your convenience!)
+- Add options object for flexibility
--- utils.js
+++ utils.js
@@ -1,88 +1,139 @@
-const fs = require("fs");
+const unicode = [
+  { isolated: "ﺁ", start: "ﺁ", middle: "ﺂ", end: "ﺂ" },
+  { isolated: "ﺃ", start: "ﺃ", middle: "ﺄ", end: "ﺄ" },
+  { isolated: "ﺇ", start: "ﺇ", middle: "ﺈ", end: "ﺈ" },
+  { isolated: "ﺍ", start: "ﺍ", middle: "ﺎ", end: "ﺎ" },
+  { isolated: "ﺏ", start: "ﺑ", middle: "ﺒ", end: "ﺐ" },
+  { isolated: "ﺕ", start: "ﺗ", middle: "ﺘ", end: "ﺖ" },
+  { isolated: "ﺙ", start: "ﺛ", middle: "ﺜ", end: "ﺚ" },
+  { isolated: "ﺝ", start: "ﺟ", middle: "ﺠ", end: "ﺞ" },
+  { isolated: "ﺡ", start: "ﺣ", middle: "ﺤ", end: "ﺢ" },
+  { isolated: "ﺥ", start: "ﺧ", middle: "ﺨ", end: "ﺦ" },
+  { isolated: "ﺩ", start: "ﺩ", middle: "ﺪ", end: "ﺪ" },
+  { isolated: "ﺫ", start: "ﺫ", middle: "ﺬ", end: "ﺬ" },
+  { isolated: "ﺭ", start: "ﺭ", middle: "ﺮ", end: "ﺮ" },
+  { isolated: "ﺯ", start: "ﺯ", middle: "ﺰ", end: "ﺰ" },
+  { isolated: "ﺱ", start: "ﺳ", middle: "ﺴ", end: "ﺲ" },
+  { isolated: "ﺵ", start: "ﺷ", middle: "ﺸ", end: "ﺶ" },
+  { isolated: "ﺹ", start: "ﺻ", middle: "ﺼ", end: "ﺺ" },
+  { isolated: "ﺽ", start: "ﺿ", middle: "ﻀ", end: "ﺾ" },
+  { isolated: "ﻁ", start: "ﻃ", middle: "ﻄ", end: "ﻂ" },
+  { isolated: "ﻅ", start: "ﻇ", middle: "ﻈ", end: "ﻆ" },
+  { isolated: "ﻉ", start: "ﻋ", middle: "ﻌ", end: "ﻊ" },
+  { isolated: "ﻍ", start: "ﻏ", middle: "ﻐ", end: "ﻎ" },
+  { isolated: "ﻑ", start: "ﻓ", middle: "ﻔ", end: "ﻒ" },
+  { isolated: "ﻕ", start: "ﻗ", middle: "ﻘ", end: "ﻖ" },
+  { isolated: "ﻙ", start: "ﻛ", middle: "ﻜ", end: "ﻚ" },
+  { isolated: "ﻝ", start: "ﻟ", middle: "ﻠ", end: "ﻞ" },
+  { isolated: "ﻡ", start: "ﻣ", middle: "ﻤ", end: "ﻢ" },
+  { isolated: "ﻥ", start: "ﻧ", middle: "ﻨ", end: "ﻦ" },
+  { isolated: "ﻩ", start: "ﻫ", middle: "ﻬ", end: "ﻪ" },
+  { isolated: "ﻭ", start: "ﻭ", middle: "ﻮ", end: "ﻮ" },
+  { isolated: "ﻱ", start: "ﻳ", middle: "ﻴ", end: "ﻲ" },
+  { isolated: "ﺓ", start: "ﺓ", middle: "ﺔ", end: "ﺔ" },
+  { isolated: "ﺅ", start: "ﺅ", middle: "ﺆ", end: "ﺆ" },
+  { isolated: "ﺉ", start: "ﺋ", middle: "ﺌ", end: "ﺊ" },
+  { isolated: "ﻯ", start: "ﻯ", middle: "ﻰ", end: "ﻰ" },
+  { isolated: "ﭖ", start: "ﭘ", middle: "ﭙ", end: "ﭗ" },
+  { isolated: "ﭺ", start: "ﭼ", middle: "ﭽ", end: "ﭻ" },
+  { isolated: "ﮊ", start: "ﮊ", middle: "ﮋ", end: "ﮋ" },
+  { isolated: "ﭪ", start: "ﭬ", middle: "ﭭ", end: "ﭫ" },
+  { isolated: "ﮒ", start: "ﮔ", middle: "ﮕ", end: "ﮓ" },
+  { isolated: "ﭦ", start: "ﭨ", middle: "ﭩ", end: "ﭧ" },
+  { isolated: "ﮦ", start: "ﮨ", middle: "ﮩ", end: "ﮧ" },
+  { isolated: "ﮮ", start: "ﮰ", middle: "ﮱ", end: "ﮯ" },
+  { isolated: "ﯼ", start: "ﯾ", middle: "ﯿ", end: "ﯽ" },
+  { isolated: "ﮈ", start: "ﮈ", middle: "ﮉ", end: "ﮉ" },
+  { isolated: "ﮌ", start: "ﮌ", middle: "ﮍ", end: "ﮍ" },
+  { isolated: "ﯗ", start: "ﯗ", middle: "ﯘ", end: "ﯘ" },
+  { isolated: "ﯙ", start: "ﯙ", middle: "ﯚ", end: "ﯚ" },
+  { isolated: "ﯛ", start: "ﯛ", middle: "ﯜ", end: "ﯜ" },
+  { isolated: "ﮎ", start: "ﮐ", middle: "ﮑ", end: "ﮏ" },
+  { isolated: "ﻵ", start: "ﻵ", middle: "ﻶ", end: "ﻶ" },
+  { isolated: "ﻷ", start: "ﻷ", middle: "ﻸ", end: "ﻸ" },
+  { isolated: "ﻹ", start: "ﻹ", middle: "ﻺ", end: "ﻺ" },
+  { isolated: "ﻻ", start: "ﻻ", middle: "ﻼ", end: "ﻼ" },
+];
 
-const alphabet = JSON.parse(
-  fs
-    .readFileSync(__dirname + "/alphabet.json", { encoding: "utf-8" })
-    .toString()
-);
+const laaIndex = unicode.length - 1;
 
-const other = {
-  lamAlif: {
-    value: "لا",
-    unicode: "\uFEFB",
-  },
-};
+var arabic = "آأإابتثجحخدذرزسشصضطظعغفقكلمنهويةؤئىپچژڤگٹہےیڈڑۇۆۈک"; // Arabic letters
 
-function rtlConverter(sentence) {
-  const sentence_ = typeof sentence === "string" ? sentence.trim() : false;
+function isHarakah(char) {
+  const harakat = ["ً", "ٞ"];
+  const isHarakah_ = char >= harakat[0] && char <= harakat[1];
+  return isHarakah_;
+}
 
-  if (sentence_) {
-    const words = sentence_.split(" ").map((word) => convertWord(word));
-    return words.reverse().join(" ");
-  } else {
-    console.log("You must provide a string. Received: " + string);
-    process.exit();
-  }
+function isAlifChar(char) {
+  const alifChars = ["آ", "أ", "إ", "ا"];
+  const isAlif = alifChars.includes(char);
+  return isAlif;
+}
+
+function isLamChar(char) {
+  const lamChars = ["ل"];
+  const isLam = lamChars.includes(char);
+  return isLam;
+}
+
+function isLinkableAfter(char) {
+  const linkableAfterChars =
+    "ٹہےڈڑگڤژچپـئؤرلالآىآةوزظشسيبللأاأتنمكطضصثقفغعهخحجدذلإإۇۆۈ";
+  return linkableAfterChars.indexOf(char) >= 0;
+}
+function isLinkableBefore(char) {
+  const linkableBeforeChars = "ٹہےگڤچپـئظشسيبلتنمكطضصثقفغعهخحج";
+  return linkableBeforeChars.indexOf(char) >= 0;
 }
 
 function convertWord(word) {
-  let letters = word.split("");
-  let word_ = "";
-  let add = 1;
-  for (let i = 0; i < letters.length; i += add) {
-    add = 1;
-    let letter = alphabet.filter((l) => l.isolated.value === letters[i])[0];
-
+  const letters = word.split("");
+  const word_ = [];
+  for (let i = 0; i < letters.length; i++) {
+    const char = letters[i];
+    const letter = unicode[arabic.indexOf(char)];
+    let isLaa = false;
+    const isEndOfArray = i === letters.length - 1;
+    const isStartOfArray = i === 0;
     // check if its لا
-    if (letters[i] === "ل" && letters[i + 1] === "ا") {
-      letters.splice(i, 2, other.lamAlif.unicode);
-      add = 2;
-      continue;
+    if (isLamChar(char) && isAlifChar(letters[i + 1])) {
+      isLaa = true;
     }
-
-    if (i === 0) {
-      let value;
-      if (!letter.beginning.unicode) {
-        value = letter.isolated.unicode;
+    if (isStartOfArray) {
+      if (isLaa) {
+        word_.push(unicode[laaIndex].start);
+      } else if (!(isLinkableBefore(char) && isLinkableAfter(char))) {
+        word_.push(letter.start);
       } else {
-        value = letter.beginning.unicode;
+        word_.push(letter.start);
       }
-
-      letters[i] = value;
-      continue;
-    }
-
-    if (!letter.middle.unicode) {
-      letters[i] = letter.end.unicode;
-      continue;
-    }
-
-    if (!letter.beginning.unicode) {
-      letters[i] = letter.isolated.unicode;
-      continue;
-    }
-
-    if (letter.isolated.value === "ه") {
-      const skip = ["ر", "ز", "ذ", ""];
-      if (letters[i - 1] === "ذ") {
-        letters[i] = letter.isolated.unicode;
+    } else if (isEndOfArray) {
+      if (isLaa) {
+        word_.push(unicode[laaIndex].end);
+      } else if (isLinkableAfter(char)) {
+        word_.push(letter.end);
+      } else {
+        word_.push(letter.isolated);
       }
-      continue;
+    } else if (isLinkableBefore(char) && isLinkableAfter(char)) {
+      if (isLaa) {
+        word_.push(unicode[laaIndex].middle);
+      } else {
+        word_.push(letter.middle);
+      }
     }
-
-    if (i === letters.length - 1) {
-      letters[i] = letter.end.unicode;
-      continue;
-    }
-
-    if (letter.middle.unicode) {
-      letters[i] = letter.middle.unicode;
-    } else {
-      letters[i] = letter.end.unicode;
+    if (isLaa) {
+      i++;
     }
   }
+  return word_.reverse().join("");
+}
 
-  return letters.reverse().join("");
+function rtlConverter(sentence) {
+  let words = sentence.split(" ");
+  words = words.map((word) => convertWord(word));
+  return words.reverse().join(" ");
 }
 
 module.exports = rtlConverter;
